e3208cd40758537e2bef562b54537d332a740a30
Ken Cox <ken@kencox.net>
2026-02-18 21:19:55 -0500

    turning css back on

--- src/App.vue
+++ src/App.vue
@@ -47,7 +47,7 @@ onMounted(() => {
 
 <style scoped>
 .no-cheating{
-  /* position: absolute;
+  position: absolute;
   left: -1000px;
   overflow: hidden;
   width: 0px;
@@ -57,6 +57,6 @@ onMounted(() => {
   border: 0;
   clip: rect(0 0 0 0);
   font-size: 0;
-  line-height: 0; */
+  line-height: 0;
 }
 </style>
